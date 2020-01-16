a50c381a00b438bc84bae929b8533fb054bea2d6
Rebeca Cruz <rebecacruz@me.com>
2020-01-15 21:50:45 -0300

    Projeto foi criado

---
+++ src/test/java/com/rebecacruz/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebecacruz.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
