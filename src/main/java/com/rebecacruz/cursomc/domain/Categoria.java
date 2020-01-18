2512c6265fb1592b1c12257e14ab16712643f6b6
Rebeca Cruz <rebecacruz@me.com>
2020-01-18 13:18:24 -0300

    Ajustes no endpoint/categorias/{id}

--- src/main/java/com/rebecacruz/cursomc/domain/Categoria.java
+++ src/main/java/com/rebecacruz/cursomc/domain/Categoria.java
@@ -3,11 +3,14 @@ package com.rebecacruz.cursomc.domain;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Categoria implements Serializable {
@@ -18,6 +21,7 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 	
+	@JsonManagedReference
 	@ManyToMany(mappedBy="categorias")
 	private List<Produto> produtos = new ArrayList<>();
 	
