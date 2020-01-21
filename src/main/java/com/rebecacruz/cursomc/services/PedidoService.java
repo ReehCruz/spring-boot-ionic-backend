c7be60f1a428813ce476987049e89e3afd1bee15
Rebeca Cruz <rebecacruz@me.com>
2020-01-20 22:58:07 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/rebecacruz/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.rebecacruz.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rebecacruz.cursomc.domain.Pedido;
+import com.rebecacruz.cursomc.repositories.PedidoRepository;
+import com.rebecacruz.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {	
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+		}
+	
+}
