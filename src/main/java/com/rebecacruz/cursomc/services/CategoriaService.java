a7a4d7cafd311d23348299d61f4712fabb182d2c
Rebeca Cruz <rebecacruz@me.com>
2020-01-26 20:20:31 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/rebecacruz/cursomc/services/CategoriaService.java
+++ src/main/java/com/rebecacruz/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rebecacruz.cursomc.domain.Categoria;
+import com.rebecacruz.cursomc.dto.CategoriaDTO;
 import com.rebecacruz.cursomc.repositories.CategoriaRepository;
 import com.rebecacruz.cursomc.services.exceptions.DataIntegrityException;
 import com.rebecacruz.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(),objDto.getNome());
 	}
 	
 }
