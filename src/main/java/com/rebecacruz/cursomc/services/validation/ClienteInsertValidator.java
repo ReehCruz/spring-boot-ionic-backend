04f4888ca95366f499cb0f8469f3a825a4d7dd5b
Rebeca Cruz <rebecacruz@me.com>
2020-01-27 19:20:31 -0300

    Validacao customizada: email nao repetido na insercao de Cliente

---
+++ src/main/java/com/rebecacruz/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,39 @@
+package com.rebecacruz.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.rebecacruz.cursomc.domain.enums.TipoCliente;
+import com.rebecacruz.cursomc.dto.ClienteNewDTO;
+import com.rebecacruz.cursomc.resources.exception.FieldMessage;
+import com.rebecacruz.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOucnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOucnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
