e86cd6b1b98867eb685d8d3ab17ccfa1e9e11b15
Rebeca Cruz <rebecacruz@me.com>
2020-01-27 10:45:56 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/rebecacruz/cursomc/resources/ClienteResource.java
+++ src/main/java/com/rebecacruz/cursomc/resources/ClienteResource.java
@@ -19,6 +19,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rebecacruz.cursomc.domain.Cliente;
 import com.rebecacruz.cursomc.dto.ClienteDTO;
+import com.rebecacruz.cursomc.dto.ClienteNewDTO;
 import com.rebecacruz.cursomc.services.ClienteService;
 
 @RestController
@@ -36,7 +37,7 @@ public class ClienteResource {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO objDto){
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){
 		Cliente obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
