569d780b88878e10a71d3f382e063372b55039e2
Rebeca Cruz <rebecacruz@me.com>
2020-01-28 23:13:50 -0300

    Busca de pedidos por nome e categorias

--- src/main/java/com/rebecacruz/cursomc/resources/PedidoResource.java
+++ src/main/java/com/rebecacruz/cursomc/resources/PedidoResource.java
@@ -16,11 +16,11 @@ public class PedidoResource {
 	
 	@Autowired
 	private PedidoService service;
-	
+
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Pedido> find(@PathVariable Integer id) {
 		Pedido obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
-		
 	}
+
 }
